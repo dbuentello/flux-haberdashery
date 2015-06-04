55a664f73d23bcb2ebba87046ffff22de86a0dfc
Daniel Buentello <dbuentello@mioty.com>
2015-06-04 12:54:50 -0500

    WIP

---
+++ assets/js/app.js
@@ -0,0 +1,14 @@
+//global namespace
+var fh = {};
+
+// misc libs
+fh.libs = {};
+
+/*
+fh.dispatcher = dispatcher
+fh.stores = collection of stores
+ */
+$(document).ready(function() {
+  console.log('ready');
+
+});
